ebcb00f96d61fa8d7bb5d7f1629a025c18b4d1c8
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-04-25 00:22:50 +0600

    Codeforces Round 867 (Div. 3)-A

--- A_TubeTube_Feed.cpp
+++ A_TubeTube_Feed.cpp
@@ -34,6 +34,109 @@ int main()
 
         int f = 0;
 
+        ll mx = -1, mx_i = -1;
+        for (int i = 0; i < q; i++)
+        {
+            if (a[i].first + i <= t && a[i].second > mx)
+            {
+                mx = a[i].second;
+                mx_i = i + 1;
+            }
+        }
+
+        cout << mx_i << endl;
+    }
+
+    return 0;
+}
+
+/*
+
+#include<bits/stdc++.h>
+#define    endl      '\n'
+#define    yes       "YES"
+#define    no        "NO"
+#define    ll        long long
+#define    fastIO    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO; //muhammad_sayem
+
+    int q;
+    cin >> q;
+
+    while(q--){
+        int n, t, i, maxB = INT_MIN, res = -1;
+        cin >> n >> t;
+        int a[n+5];
+        int b[n+5];
+
+        for(i=1; i<=n; i++){
+            cin >> a[i];
+        }
+
+        for(i=1; i<=n; i++){
+            cin >> b[i];
+        }
+
+        for(i=1; i<=n; i++){
+            if(a[i]+i-1 <= t && b[i] > maxB){
+                maxB = b[i];
+                res = i;
+            }
+        }
+
+        cout << res << endl;
+
+    }
+
+    return 0;
+}
+
+
+
+
+*/
+
+/*
+
+#include <bits/stdc++.h>
+#define ll long long
+#define pii pair<int, int>
+#define sz(v) v.size()
+#define mem(a, x) memset(a, x, sizeof(a))
+#define fio                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0)
+using namespace std;
+int main()
+{
+    fio;
+
+    int test;
+    cin >> test;
+
+    while (test--)
+    {
+        int q, t;
+        cin >> q >> t;
+
+        pii a[q + 1];
+
+        for (int i = 0; i < q; i++)
+        {
+            cin >> a[i].first;
+        }
+
+        for (int i = 0; i < q; i++)
+        {
+            cin >> a[i].second;
+        }
+
+        int f = 0;
+
         for (int i = 0; i < q; i++)
         {
             if (a[i].first+i <= t)
@@ -50,7 +153,7 @@ int main()
             ll mx = -1,mx_i=-1;
             for (int i = 0; i < q; i++)
             {
-                if (a[i].first+i <= t && a[i].second >= mx)
+                if (a[i].first+i <= t && a[i].second > mx) //&& a[i].second >= mx dileu no problem
                 {
                     mx = a[i].second;
                     mx_i = i+1;
@@ -63,3 +166,5 @@ int main()
 
     return 0;
 }
+
+*/
