d40b15ca0c8c30517f06de8a17817c3d1fb9f6d6
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-05-02 01:00:18 +0600

    Codeforces Round 438 by Sberbank and Barcelona Bootcamp (Div. 1 + Div. 2 combined), problem: (A) Bark to Unlock

--- A_Bark_to_Unlock.cpp
+++ A_Bark_to_Unlock.cpp
@@ -1,4 +1,49 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define      endl            '\n'
+#define      yes             "YES"
+#define      no              "NO"
+#define      ll              long long
+#define      pii             pair <int,int>
+#define      pb              push_back
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO; //muhammad_sayem
+
+    string str;
+    cin >> str;
+    int n, cnt1 = 0, cnt2 = 0;
+    cin >> n;
+
+    while(n--){
+        string s;
+        cin >> s;
+
+        if(s[0] == str[0] && s[1] == str[1]) {
+            cnt1 = 1;
+            cnt2 = 1;
+        }
+
+        else{
+            if(s[0] == str[1]) cnt1++;
+            if(s[1] == str[0]) cnt2++;
+        }
+
+    }
+
+    if(cnt1 >= 1 && cnt2 >= 1) cout << yes << endl;
+    else cout << no << endl;
+
+    return 0;
+}
+
+
+
+
+/* #include <bits/stdc++.h>
 #define ll long long
 #define pii pair<int, int>
 #define sz(v) v.size()
@@ -87,4 +132,4 @@ int main()
     
 
     return 0;
-}
+} */
