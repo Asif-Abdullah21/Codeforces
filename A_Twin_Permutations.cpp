a7dd03b59e4ceb1a09f0c0b02eb6eb14ea71783a
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-05-29 00:24:38 +0600

    Codeforces Round 875 (Div. 2), problem: (A) Twin Permutations

--- A_Twin_Permutations.cpp
+++ A_Twin_Permutations.cpp
@@ -34,15 +34,10 @@ int main()
        {
            cin >> a[i];
        }
-       
-
-       int mx = *max_element(a,a+n);
-
-       int val = mx+1;
 
        for (int i = 0; i < n; i++)
        {
-           cout << val - a[i] << " ";
+           cout << n+1 - a[i] << " ";
        }
 
        cout << endl;
@@ -51,3 +46,59 @@ int main()
      
     return 0;
 }
+
+
+
+// #include <bits/stdc++.h>
+// #define endl "\n"
+// #define nn (cout << "NO\n")
+// #define yy (cout << "YES\n")
+// #define ss " "
+// #define sort(a) sort(a,a+n)
+// #define rev(a) reverse(a,a+n)
+// #define ll long long
+// #define ull unsigned long long
+// #define gcd(a, b) __gcd(a, b)
+// #define lcm(a, b) ((a / gcd(a, b)) * b)
+// #define pii pair<int, int>
+// #define pll pair<long long, long long>
+// #define sz(v) v.size()
+// #define mm(a, x) memset(a, x, sizeof(a))
+// #define FIO                       \
+//     ios_base::sync_with_stdio(0); \
+//     cin.tie(0);                   \
+//     cout.tie(0);
+// using namespace std;
+// int main()
+// {
+//     FIO;
+//     int t;
+//     cin >> t;
+
+//     while (t--)
+//     {
+//        int n;
+//        cin >> n;
+
+//        int a[n];
+//        for (int i = 0; i < n; i++)
+//        {
+//            cin >> a[i];
+//        }
+       
+
+//        int mx = *max_element(a,a+n);
+
+//        int val = mx+1;
+
+//        for (int i = 0; i < n; i++)
+//        {
+//            cout << val - a[i] << " ";
+//        }
+
+//        cout << endl;
+       
+//     }
+     
+//     return 0;
+// }
