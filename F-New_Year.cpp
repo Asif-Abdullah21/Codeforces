1534d4175595806e1963a4561e39175896c3cb62
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-06-09 17:55:32 +0600

    Codeforces Round 611 (Div. 3), problem: (A) Minutes Before the New Year

---
+++ F-New_Year.cpp
@@ -0,0 +1,76 @@
+// #include <bits/stdc++.h>
+// #define endl "\n"
+// #define nn (cout << "NO\n")
+// #define yy (cout << "YES\n")
+// #define ll long long
+// #define ull unsigned long long
+// #define gcd(a, b) __gcd(a, b)
+// #define lcm(a, b) ((a / gcd(a, b)) * b)
+// #define pii pair<int, int>
+// #define pll pair<long long, long long>
+// #define sz(v) v.size()
+// #define mm(a, x) memset(a, x, sizeof(a))
+// #define FIO                       \
+//     ios_base::sync_with_stdio(0); \
+//     cin.tie(0);                   \
+//     cout.tie(0);
+// using namespace std;
+// int main()
+// {
+//     FIO;
+//     int t;
+//     cin >> t;
+
+//     while (t--)
+//     {
+//         int h,m;
+//         cin >> h >>m;
+
+//         int minute = 60-m;
+//         int hour = 24-(h+1);
+//         minute += (hour*60);
+
+//         cout << minute << endl;
+//     }
+     
+//     return 0;
+// }
+
+
+#include <bits/stdc++.h>
+#define endl "\n"
+#define nn (cout << "NO\n")
+#define yy (cout << "YES\n")
+#define ll long long
+#define ull unsigned long long
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a / gcd(a, b)) * b)
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define sz(v) v.size()
+#define mm(a, x) memset(a, x, sizeof(a))
+#define FIO                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+using namespace std;
+int main()
+{
+    FIO;
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int h,m;
+        cin >> h >>m;
+        int ans =0;
+        int hour = 24-h;
+        hour = hour*60;
+        ans = hour - m;
+
+        cout << ans << endl;
+    }
+     
+    return 0;
+}
