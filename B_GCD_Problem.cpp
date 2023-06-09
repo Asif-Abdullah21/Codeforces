7aa94e69f25a73e509c268e59bffdf7238f6b322
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-06-10 00:21:12 +0600

    Codeforces Round 761 (Div. 2), problem: (B) GCD Problem

---
+++ B_GCD_Problem.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define nn (cout << "NO\n")
+#define yy (cout << "YES\n")
+#define ll long long
+#define ull unsigned long long
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a / gcd(a, b)) * b)
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define sz(v) v.size()
+#define mm(a, x) memset(a, x, sizeof(a))
+#define FIO                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+using namespace std;
+int main()
+{
+    FIO;
+    int t; cin >> t;
+
+    while (t--)
+    {
+        int n,m;
+        cin >> n;   
+
+        if (n % 2 == 0)
+            cout << n - 3 << " " << 2 << " "<< 1 << endl;
+        else
+        {
+            n = n-1;   //code of this else part is explained by muhammad_sayem
+            m = n/2;
+
+            if(m%2==0) cout << m+1 << " " << m-1 << " " << 1 << endl;
+            else cout << m+2 << " " << m-2 << " " << 1 << endl; 
+        }
+    }
+
+    return 0;
+}
