f8f754ebc1b23848308e62dcfa81a7bc1a03334d
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-06-10 19:17:56 +0600

    Codeforces Global Round 7, problem: (A) Bad Ugly Numbers

--- A_Bad_Ugly_Numbers.cpp
+++ A_Bad_Ugly_Numbers.cpp
@@ -51,46 +51,43 @@ int main()
 
 
 
-
-
-
-#include<bits/stdc++.h>
-#define      endl            '\n'
-#define      yes             "YES"
-#define      no              "NO"
-#define      ll              long long
-#define      pb              push_back
-#define      pii             pair <int,int>
-#define      pll             pair <long long,long long>
-#define      rev(a)          reverse(a, a+n);
-#define      Srev(s)         reverse(s.begin(), s.end());
-#define      sz(s)           s.size()
-#define      gcd(a,b)        __gcd(a,b)
-#define      lcm(a,b)        (a*b)/gcd(a,b)
-#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-using namespace std;
-int main()
-{
-    fastIO;             // Polash_Islam vai's logic //
+// #include<bits/stdc++.h>
+// #define      endl            '\n'
+// #define      yes             "YES"
+// #define      no              "NO"
+// #define      ll              long long
+// #define      pb              push_back
+// #define      pii             pair <int,int>
+// #define      pll             pair <long long,long long>
+// #define      rev(a)          reverse(a, a+n);
+// #define      Srev(s)         reverse(s.begin(), s.end());
+// #define      sz(s)           s.size()
+// #define      gcd(a,b)        __gcd(a,b)
+// #define      lcm(a,b)        (a*b)/gcd(a,b)
+// #define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+// using namespace std;
+// int main()
+// {
+//     fastIO;             // Polash_Islam vai's logic //
     
-    int test;
-    cin >> test;
+//     int test;
+//     cin >> test;
 
-    while(test--){
-        int n, i;
-        cin >> n;
+//     while(test--){
+//         int n, i;
+//         cin >> n;
 
-        if(n==1) cout << -1 << endl;
+//         if(n==1) cout << -1 << endl;
 
-        else{
-            for(i=1; i<=n-1; i++){
-                cout << 7; 
-            }
-            cout << 4 << endl;
+//         else{
+//             for(i=1; i<=n-1; i++){
+//                 cout << 7; 
+//             }
+//             cout << 4 << endl;
         
-        }
-    }
+//         }
+//     }
 
 
-    return 0;
-}
+//     return 0;
+// }
