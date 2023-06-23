2ecd270fee0c3f0b0600f6d59b452ebe6311a278
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-06-23 18:17:33 +0600

    Educational Codeforces Round 116 (Rated for Div. 2), problem: (A) AB Balance

--- A_AB_Balance.cpp
+++ A_AB_Balance.cpp
@@ -23,71 +23,11 @@ int main()
 
     while (t--)
     {
-        FIO;
         string s;
         cin >> s;
-        int ab = 0, ba = 0;
-        string s1 = "";
 
-        for (int i = 0; i < s.size() - 1; i++)
-        {
-
-            s1 += s[i];
-            s1 += s[i + 1];
-
-            if (s1 == "ab")
-                ab++;
-
-            else if (s1 == "ba")
-                ba++;
-        }
-
-        int cnt1 = 0, cnt2 = 0,f=0;
-
-        int val = abs(ab - ba);
-
-        if (ab == ba)
-            cout << s << endl;
-        else if (ab > ba)
-        {
-            if (s[0] == 'a' && s[1] == 'b')
-            {
-                s[0] = 'b';
-                cnt1++;
-            }
-            if (cnt1 == val)
-             {
-                   cout << s << endl;
-                   f=1;
-             }
-            else
-            {
-                for (int i = 2; i < s.size(); i++)
-                {
-                    if (s[i] == 'a' && s[i + 1] == 'b' && s[i - 1] == 'b')
-                    s[i] = 'b';
-                    cnt1++;
-                    if(cnt1==val)
-                    {
-                        cout << s << endl;
-                        f=1;
-                        break;
-                    }
-                }
-            }
-        }
-
-        else if (ba > ab)
-        {
-            if (s[1] == 'a' && s[1] == 'b')
-                cnt2++;
-
-            for (int i = 2; i < s.size(); i++)
-            {
-                if (s[i] == 'a' && s[i + 1] == 'b' && s[i - 1] == 'b')
-                    cnt2++;
-            }
-        }
+        if(s[0]!=s[sz(s)]-1) s[0] = s[sz(s)-1];
+        cout << s<< endl;
     }
 
     return 0;
